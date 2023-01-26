432fceaa38c02c43ad3a2cd4c9030002af1b9c8b
Sina <sadeghesfahani.sina@gmail.com>
2023-01-26 07:18:23 +0330

    feat(tests): test fundamentals implemented

--- calculation/tests.py
+++ calculation/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APIClient
+
+from .views import CalculationView
+
+requests = APIRequestFactory()
+
+
+class CalculationTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        super().setUp()
+
+    def testAddEndpoint(self):
+        payload = {"first": 1, "second": 2}
+        request = requests.post("/calculation/add", data=json.dumps(payload), content_type='application/json')
+        response = CalculationView.as_view({'post': 'add'})(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {"result": 3})
+
